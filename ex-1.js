6462093720facd81c5913a7dea42391aa59b6f3b
Jed <alskoo35@gmail.com>
2023-12-06 23:37:26 +0700

    Jed

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,20 @@
-// Start coding here
+function add (a,b){
+    return a+b;
+}
+function subtract (a,b){
+    return a-b;
+}
+function multiply (a,b){
+    return a*b;
+}
+function divide (a,b){
+    return a/b;
+}
+let testadd= add(10,5)
+console.log(testadd)
+let testsubtract= subtract(10,5)
+console.log(testsubtract)
+let testmultiply= multiply(10,5)
+console.log(testmultiply)
+let testdivide= divide(10,5)
+console.log(testdivide)
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,20 @@
-// Start coding here
+let add = function (a,b){
+    return a+b;
+}
+let subtract = function (a,b){
+    return a-b;
+}
+let multiply = function (a,b){
+    return a*b;
+}
+let divide = function (a,b){
+    return a/b;
+}
+let testadd= add(10,5)
+console.log(testadd)
+let testsubtract= subtract(10,5)
+console.log(testsubtract)
+let testmultiply= multiply(10,5)
+console.log(testmultiply)
+let testdivide= divide(10,5)
+console.log(testdivide)
